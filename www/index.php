14d311e529dc3d05ecbfa3c25745bafdac160ce0
cyxapuk <pavel.yakushenkov@gmail.com>
2013-10-02 15:29:44 +0400

    a

--- www/index.php
+++ www/index.php
@@ -1,10 +1,11 @@
 <?php
 
+if($_SERVER['REMOTE_ADDR'] != '78.140.146.70')
+{
+    die("Сайт находится в стадии разработки");
+}
+
 include_once('../bin/starter.html');
-
-echo ">" . $_SERVER['REMOTE_ADDR'];
-exit;
-
 
 $controller->displayPage();
 
